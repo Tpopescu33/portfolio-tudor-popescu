69ee4f18093db0672f0484e98115b6e34b04a0d0
tpopescu33 <tudor.s.popescu@gmail.com>
2022-11-05 15:40:49 -0500

    test

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-600'>
             Work
           </p>
           <p className='py-6'>// Check out some of my recent work</p>
@@ -36,7 +36,7 @@ const Projects = () => {
       </span>
       <div className="pt-8 text-center ">
         {/* eslint-disable-next-line */}
-        <a href={item.github} target="_blank">
+        <a href={item.github} >
           <button
             className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
@@ -51,6 +51,14 @@ const Projects = () => {
                        bg-white text-gray-700 font-bold text-lg"
           >
             Live
+          </button>
+        </a>
+        <a target="_blank">
+          <button
+            className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+          >
+            Info
           </button>
         </a>
       </div>
