ee800b9f2f2ee1c7d8110ee97d2b4c3cb1ee7981
tpopescu33 <tudor.s.popescu@gmail.com>
2022-11-05 13:17:21 -0500

    view projects button links to projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -12,15 +13,17 @@ const Home = () => {
         </h1>
         
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a software developer specializing in building and designing full-stack web applications, mobile applications.
+          I’m a software developer specializing in building and designing full-stack web applications and mobile applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
+        <Link to='work' smooth={true} duration={500}>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600'>
+            View Projects
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
           </button>
+          </Link>
         </div>
       </div>
     </div>
