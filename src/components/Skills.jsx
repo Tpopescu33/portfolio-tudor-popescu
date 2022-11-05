28f371f064f1b90a82d38f594cf6a0c8b069eb80
tpopescu33 <tudor.s.popescu@gmail.com>
2022-11-05 14:54:53 -0500

    added more skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,11 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
+import AWS from '../assets/aws.png';
+import Cpp from '../assets/cpp.png';
+import Swift from '../assets/swift.webp';
+import SQL from '../assets/sql.png';
+import Java from '../assets/java.png';
 
 
 const Skills = () => {
@@ -16,7 +21,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-yellow-600 '>Skills</p>
-              <p className='py-4'>// These are the technologies I've worked with</p>
+              <p className='py-4'>// These are some of the technologies I've worked with</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
@@ -47,6 +52,26 @@ const Skills = () => {
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
                   <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
+                  <p className='my-4'>AWS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Cpp} alt="HTML icon" />
+                  <p className='my-4'>C++</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={SQL} alt="HTML icon" />
+                  <p className='my-4'> MYSQL</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Swift} alt="HTML icon" />
+                  <p className='my-4'>Swift</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Java} alt="HTML icon" />
+                  <p className='my-4'>Java</p>
               </div>
           </div>
       </div>
