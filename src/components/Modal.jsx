2765deede279914815874291872cf9f41cc03a6b
tpopescu33 <tudor.s.popescu@gmail.com>
2022-11-24 13:47:24 -0600

    exit animation needed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import { motion } from "framer-motion";
+import Backdrop from './Backdrop';
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
 
 
 const Modal = ({ open, onClose, name, info }) => {
   if (!open) return null;
   return (
-    <div onClick={onClose} className='overlay'>
-      <div
+
+     
+    <Backdrop onClick={onClose}>
+      <motion.div
         onClick={(e) => {
           e.stopPropagation();
         }}
         className='modalContainer'
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+
       >
         
         <div className='modalRight'>
@@ -25,8 +55,9 @@ const Modal = ({ open, onClose, name, info }) => {
             
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </Backdrop>
+    
   );
 };
 
